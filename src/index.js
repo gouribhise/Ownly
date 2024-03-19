b291419e3696c9364dcd9e85b02ac9f545632f12
gouribhise <gouri.a.bhise@gmail.com>
2024-03-19 19:35:27 +0530

    added login and logout functionality using auth0

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-2shzi5aba67i3u2e.us.auth0.com"
+    clientId="i6nKJHZ42uSoPA5qb31HIPW1LB4uUZTE"
+    redirectUri={window.location.origin}
+    >
     <App />
+    </Auth0Provider>
+
   </React.StrictMode>
 );
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import { useAuth0 } from '@auth0/auth0-react';
 const Home = () => {
-  return <div>Home</div>;
+  const {loginWithRedirect,isAuthenticated,user,logout}=useAuth0()
+  const isUser=isAuthenticated && user
+
+  return <div> 
+  {isUser?    <button onClick={()=>{logout({returnTo:window.location.orogin})}}>logout</button>
+:<button onClick={loginWithRedirect}>login</button>}
+  </div>;
 };
 
 export default Home;
