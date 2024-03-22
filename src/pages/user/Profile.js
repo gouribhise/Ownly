dd2e5410bef508167090b1061cda1e0f3249c75e
gouribhise <gouri.a.bhise@gmail.com>
2024-03-23 01:05:30 +0530

    added simple form for user profile

--- src/pages/user/Profile.js
+++ src/pages/user/Profile.js
@@ -1,6 +1,29 @@
+import { useState } from "react"
+
+
 const Profile=()=>{
+    const[name,setName]=useState('')
+    const [email,setEmail]=useState('')
+    const [address,setAddress]=useState('')
+    const [mblno,setMblno]=useState(0)
     return (
-        <h2>Profile</h2>
+         <div className="container">
+            <div className="row">
+                <div className="col-5 mx-auto">
+                    <h5>User Info</h5>
+                    <form>
+                      <label>Name</label> &nbsp;
+                      <input  className="form-control" type="text" name="name" value={name}/><br/>    
+                      <label>Email</label> &nbsp;
+                      <input className="form-control" type="email" name="email" value={email}/><br/>
+                      <label>Address</label> &nbsp;
+                      <input className="form-control" type="textarea" name="address" value={address}/><br/>
+                      <label>Mbl. No.</label> &nbsp;
+                      <input className="form-control" type="number" name="mblnumber" value={mblno}/>
+                    </form>
+                </div>
+            </div>
+         </div>
     )
 }
 
